c431e597e5174d2c0773bdac461e582ee5c7d82a
didemertens <d.g.j.mertens@gmail.com>
2020-04-15 12:19:33 +0100

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,21 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import Enzyme, { shallow } from 'enzyme'
+import EnzymeAdapater from 'enzyme-adapter-react-16'
+import App from './App'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+Enzyme.configure({ adapter: new EnzymeAdapater() })
+
+const setup = () => {
+  const wrapper = shallow(<App />)
+  return wrapper
+}
+
+const findByTestAttr = (wrapper, val) => {
+  return wrapper.find(`[data-test="${val}"]`)
+}
+
+test('renders component without error', () => {
+  const wrapper = setup()
+  const appComponent = findByTestAttr(wrapper, 'app-component')
+  expect(appComponent.length).toBe(1)
+})
